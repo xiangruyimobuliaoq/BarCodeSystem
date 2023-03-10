eddc06db56430227baf6e979b1fe5c7f56a261d5
44697 <446971874@qq.com>
2023-03-10 16:55:29 +0800

    L2版本

--- app/src/main/java/com/isl/bcs/view/DBPullActivity.kt
+++ app/src/main/java/com/isl/bcs/view/DBPullActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import com.drake.net.Get
 import com.drake.net.utils.scopeNetLife
 import com.drake.net.utils.withIO
+import com.drake.net.utils.withMain
 import com.drake.serialize.intent.openActivity
 import com.drake.statusbar.immersive
 import com.isl.bcs.R
@@ -13,6 +14,8 @@ import com.isl.bcs.base.BaseActivity
 import com.isl.bcs.databinding.ActivityDbpullBinding
 import com.isl.bcs.model.*
 import com.isl.bcs.utils.Constants
+import kotlinx.coroutines.Dispatchers
+import okhttp3.Dispatcher
 import org.litepal.LitePal
 import org.litepal.extension.deleteAll
 import org.litepal.extension.saveAll
@@ -36,33 +39,33 @@ class DBPullActivity : BaseActivity() {
                 Constants.TYPE_TRANSFER -> ivIntentTag.setImageResource(R.mipmap.home_icon_2)
             }
             btnDownload.setOnClickListener {
-                scopeNetLife {
-                    btnDownload.visibility = View.GONE
-                    tvStatus.visibility = View.VISIBLE
-                    progressBar.visibility = View.VISIBLE
+                btnDownload.visibility = View.GONE
+                tvStatus.visibility = View.VISIBLE
+                progressBar.visibility = View.VISIBLE
+                scopeNetLife(dispatcher = Dispatchers.IO) {
                     val staffList = Get<List<Staff>?>("/master/staff")
                     val whList = Get<List<Warehouse>?>("/master/warehouse")
                     val instItemList = Get<List<InstItemOut>?>("/scan/stock")
                     val instItemCheck = Get<List<InstItemCheck>?>("/scan/stock_check")
-                    withIO {
-                        LitePal.deleteAll<BoxIn>()
-                        LitePal.deleteAll<BoxOut>()
-                        LitePal.deleteAll<BoxTransfer>()
-                        LitePal.deleteAll<InstTrxIn>()
-                        LitePal.deleteAll<InstTrxOut>()
-                        LitePal.deleteAll<Staff>()
-                        LitePal.deleteAll<Warehouse>()
-                        LitePal.deleteAll<InstItemOut>()
-                        LitePal.deleteAll<InstItemCheck>()
-                        staffList.await()?.saveAll()
-                        whList.await()?.saveAll()
-                        instItemList.await()?.saveAll()
-                        instItemCheck.await()?.saveAll()
+                    LitePal.deleteAll<BoxIn>()
+                    LitePal.deleteAll<BoxOut>()
+                    LitePal.deleteAll<BoxTransfer>()
+                    LitePal.deleteAll<InstTrxIn>()
+                    LitePal.deleteAll<InstTrxOut>()
+                    LitePal.deleteAll<Staff>()
+                    LitePal.deleteAll<Warehouse>()
+                    LitePal.deleteAll<InstItemOut>()
+                    LitePal.deleteAll<InstItemCheck>()
+                    staffList.await()?.saveAll()
+                    whList.await()?.saveAll()
+                    instItemList.await()?.saveAll()
+                    instItemCheck.await()?.saveAll()
+                    withMain {
+                        btnNext.visibility = View.VISIBLE
+                        tvStatus.text = getString(R.string.complete)
+                        progressBar.isIndeterminate = false
+                        progressBar.progress = 100
                     }
-                    btnNext.visibility = View.VISIBLE
-                    tvStatus.text = getString(R.string.complete)
-                    progressBar.isIndeterminate = false
-                    progressBar.progress = 100
                 }
             }
             btnNext.setOnClickListener {
